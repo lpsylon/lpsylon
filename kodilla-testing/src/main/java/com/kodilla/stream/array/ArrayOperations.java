0fe027569676891519bf3f851ca99a2c0066fe0e
tai hudeev <jimdandy.hectic@gmail.com>
2021-08-21 00:48:53 +0200

    array

---
+++ kodilla-testing/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import java.util.Iterator;
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+    static double getAverage(int[] numbers){
+        IntStream.range(0,numbers.length)
+                .mapToObj(n->numbers[n])
+                .forEach((System.out::print));
+        System.out.println();
+        double result=IntStream.range(0,numbers.length)
+                .mapToDouble(n->numbers[n])
+                .average().getAsDouble();
+        return result;
+
+    }
+}
