d41ae82c2f625845af7df5ddcb0f17e2b9ba6da3
tai hudeev <jimdandy.hectic@gmail.com>
2021-08-20 06:53:02 +0200

    Beautifier

---
+++ kodilla-testing/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,10 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+
+
+    public String beautify( String  text, PoemDecorator decorator){
+
+        return decorator.decorate(text);
+    }
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.beautifier;
+
+public class StreamMain {
+
+
+    public static void main(String[] args){
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        PoemDecorator add_exclam = (s) -> s + "!";
+        PoemDecorator add_ask = (s) -> s + "?";
+        PoemDecorator toUpper = String::toUpperCase;
+
+        System.out.println( poemBeautifier.beautify("addBeau", add_exclam ));
+        System.out.println( poemBeautifier.beautify( "addBeau", add_ask ));
+        System.out.println( poemBeautifier.beautify( "addBeau", toUpper ));
+
+    }
+}
