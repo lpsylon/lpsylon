95d679bb8c0db8b34cd5bc6d89322f58abe4e7b7
tai hudeev <jimdandy.hectic@gmail.com>
2021-08-20 08:44:00 +0200

    immutable

--- kodilla-testing/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -3,31 +3,16 @@ package com.kodilla.stream.beautifier;
 public class StreamMain {
 
 
-<<<<<<< HEAD
     public static void main(String[] args){
-=======
-    public static void main(String args[]){
->>>>>>> 60f74a3eb4a187ed05063707b78f2b99d6fd3518
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         PoemDecorator add_exclam = (s) -> s + "!";
         PoemDecorator add_ask = (s) -> s + "?";
-<<<<<<< HEAD
         PoemDecorator toUpper = String::toUpperCase;
-=======
-        PoemDecorator toUpper = (s) -> s.toUpperCase();
->>>>>>> 60f74a3eb4a187ed05063707b78f2b99d6fd3518
 
         System.out.println( poemBeautifier.beautify("addBeau", add_exclam ));
         System.out.println( poemBeautifier.beautify( "addBeau", add_ask ));
         System.out.println( poemBeautifier.beautify( "addBeau", toUpper ));
 
     }
-<<<<<<< HEAD
 }
-=======
-
-
-
-}
->>>>>>> 60f74a3eb4a187ed05063707b78f2b99d6fd3518
--- kodilla-testing/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -16,4 +16,5 @@ public class ForumUser {
     public String getRealName() {
         return realName;
     }
+
 }
